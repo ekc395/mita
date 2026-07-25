8007cd5adbac13fd56a0a72001e4a8008052b6e9
ekc395 <ekc395@gmail.com>
2026-07-24 23:41:33 -0700

    initial scaffold

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "s4.anilist.co" },
+      { protocol: "https", hostname: "img.anili.st" },
+    ],
+  },
+};
+
+export default nextConfig;
